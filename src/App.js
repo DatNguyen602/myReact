053751dca7744960cdb0dbf000965f009a268467
DatNguyen602 <ngoclanhlethi2@gmail.com>
2023-03-27 18:21:33 +0700

    code41

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import HeaderBanner from './HeaderBanner';
-import Footer from './Footer';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <HeaderBanner/>
-      <Footer/>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
