b35a58ca875c310be078a26067fc87784f66d150
DatNguyen602 <ngoclanhlethi2@gmail.com>
2023-04-08 19:45:11 +0700

    code4-194408042023

--- src/Components/UseEffect.jsx
+++ src/Components/UseEffect.jsx
@@ -2,32 +2,32 @@ import React, { useEffect, useState } from "react";
 import "./style.css";
 
 const UseEffect = () => {
-  const [post, setPosts] = useState([]);
-  const [count, setCount] = useState(1);
+  const [getData, setGetData] = useState([]);
+  const [getMore, setGetMore] = useState(1);
 
   const handleUp = () => {
-    setCount(count + 1);
+    setGetMore(getMore + 1);
   };
 
   useEffect(() => {
-    fetch(`https://picsum.photos/v2/list?page=0&limit=${count * 8}`)
+    fetch(`https://picsum.photos/v2/list?page=0&limit=${getMore * 8}`)
       .then((res) => res.json())
-      .then((posts) => {
-        setPosts(posts);
+      .then((getDatas) => {
+        setGetData(getDatas);
       });
-  }, [count]);
+  }, [getMore]);
 
   return (
     <div>
       <div className="flex-s">
-        {post.length &&
-          post.map((postIT, index) => {
+        {getData.length &&
+          getData.map((data) => {
             // const style ;
 
             return (
-              <div key={postIT.id}>
-                {console.log(postIT.download_url)}
-                <img src={postIT.download_url} className="s-img" alt="Anh" />
+              <div key={data.id}>
+                {console.log(data.download_url)}
+                <img src={data.download_url} className="s-img" alt="Anh" />
               </div>
             );
           })}
