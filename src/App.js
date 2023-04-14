91ce895f6f62d145a5d2047368f99dbf305f0baa
DatNguyen602 <ngoclanhlethi2@gmail.com>
2023-04-14 12:35:33 +0700

    code5 12:36 14-04-2023

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import TodoLists from "./Components/TodoLists.jsx"
 
 function App() {
   return (
     <div className="App">
+      <TodoLists/>
     </div>
   );
 }
---
+++ src/Components/TodoLists.jsx
@@ -0,0 +1,83 @@
+import React , {useRef,useState,useEffect} from "react";
+
+const TodoLists = () => {
+    const [List,setList] = useState([]);
+    const TodoRef = useRef();
+    const updateRef = useRef();
+    const [ping,setPing] = useState(-1);
+
+    const handleAddList = () => {
+        if(TodoRef.current.value !== ""){
+            const todo = TodoRef.current.value;
+            setList([todo,...List]);
+            TodoRef.current.value = "";
+        }
+    }
+
+    const handleDel = (index , array)=>{
+        return () => {
+            const newL = array.filter((value,id) => {
+                return id !== index;
+            })
+            setList(newL);
+        }
+    }
+    
+    const Form = () => {
+        return <div className="btnIn">
+            <input ref={updateRef} type="text" className="BorderOfUpdate" placeholder="Add a todo" id="InputTodo" />
+            <button className= "ColorOfUpdate" onClick={handleUpDate}>Add Todo</button>
+        </div>
+    }
+
+    const clickToUpdate = (index)=> {
+        return () => {
+            setPing(index);
+        }
+    }
+    
+    const handleUpDate = () => {
+        if(updateRef.current.value !== List[ping] && updateRef.current.value !== ""){
+            List[ping] = updateRef.current.value;
+        }
+        setPing(-1);
+    }
+
+    useEffect (()=>{
+        if (ping !== -1) {
+            updateRef.current.value = List[ping];
+            updateRef.current.focus();
+        }
+    },[ping])
+
+    const TodoList = () =>{
+        return <React.Fragment>
+            {List.map((value,index,array)=>{
+            return <div key={Math.random()} className="List">
+                    <p>{value}</p>
+                        <div>
+                            <button onClick={handleDel(index,array)}><i className="fa-regular fa-trash"></i></button>
+                            <button onClick={clickToUpdate(index)}>V</button>
+                        </div>
+                    </div>
+            })}
+        </React.Fragment>
+    }
+
+
+    return <div className="set-backG">
+        <div className="container">
+            <h1>What's the Plan for Today</h1>
+            <div className="btnIn">
+                <input ref={TodoRef} type="text" placeholder="Add a todo" id="InputTodo" />
+                <button  onClick={handleAddList}>Add Todo</button>
+            </div>
+            {ping === -1 ? <TodoList/> : <Form/>}
+            
+        </div>
+
+        
+    </div>
+}
+
+export default TodoLists;
